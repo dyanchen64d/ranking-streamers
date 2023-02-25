1c9d80de9e83daf47cdd6cc91672e0999cd32645
chen.zhang <zhangchen64d@gmail.com>
2023-02-25 22:15:50 +0900

    upate

--- src/components/RankStreamer.jsx
+++ src/components/RankStreamer.jsx
@@ -3,27 +3,34 @@ import { useEffect, useState } from 'react';
 const RankStreamer = ({ data }) => {
   const [list, setList] = useState(data);
 
-  // useEffect(() => {
-  //   console.log('RankStreamer', data);
-  // }, [data]);
+  // sort
+  useEffect(() => {
+    const mock = [...list];
+    mock.sort((a, b) => b.score - a.score);
+    setList(mock);
+  }, [data]);
 
   return (
-    <>
+    <div className="wrapper">
       <div className="item">
         <div>Rank</div>
         <div>Name</div>
         <div>Score</div>
       </div>
-      {data.map((item, idx) => {
+      {list.map((item, idx) => {
         return (
-          <div key={item.id} className="item">
+          <div
+            key={item.id}
+            className="item"
+            style={{ top: `${24 * (idx + 1)}px` }}
+          >
             <div>{idx + 1}</div>
             <div>{item.name}</div>
             <div>{item.score}</div>
           </div>
         );
       })}
-    </>
+    </div>
   );
 };
 
